c309b931f61c835600a5b3d8d45b77efd94043c6
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-09-08 22:19:31 -0400

    Sample instruments added, able to click to change URL (although not correctly)

---
+++ app/app.module.js
@@ -0,0 +1,25 @@
+var instrumentApp = angular.module ('instrumentApp', []);
+
+instrumentApp.controller ('InstrumentCtrl', function ($scope, $location) {
+	$scope.instruments = [
+		{
+			name: 'Clarinet', 
+			url: 'clarinet'
+		},
+		{
+			name: 'Trumpet', 
+			url: 'trumpet'
+		},
+		{
+			name: 'Guitar', 
+			url: 'guitar'
+		}
+	];
+
+	$scope.go = function (path) {
+		$location.path(path);
+	};
+
+});
+
+
