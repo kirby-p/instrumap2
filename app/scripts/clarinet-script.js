6b410a78e09630a6a885fbaccb906d1b7b637b3b
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-09-17 08:19:05 -0400

    Overlay graphics now change with button click

--- app/scripts/clarinet-script.js
+++ app/scripts/clarinet-script.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
 	console.log("clarinet script loaded...");	
 
 	$(".clarinet-note-buttons").on("click", function() {
+      $(".overlay").hide();
 	  switch($(this).attr("id")) {
 	    case "button-clarinet-C":
-	      $("#img-clarinet-C").css("display", "block");
+	      $("#img-clarinet-C").show();
 	      break;
 	    case "button-clarinet-D":
-	      console.log("D note for Clarinet");
+	      $(".overlay").hide();
 	      break;    
 	  	case "button-clarinet-E":
-	      console.log("E note for Clarinet");
+	      $(".overlay").css("display", "hidden");
 	      break;
 	  }
 	})
