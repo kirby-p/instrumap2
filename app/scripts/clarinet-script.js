7e449f1f943bbe3c71f7767c4696ab9162d0aa84
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-09-20 14:43:53 -0400

    Added note overlay assets

--- app/scripts/clarinet-script.js
+++ app/scripts/clarinet-script.js
@@ -8,16 +8,49 @@ $(document).ready(function() {
 		    	console.log("C note");
 		    	$("#img-clarinet-C").show();
 				break;
+		    case "button-clarinet-C#":
+		    	console.log("C# note");
+		    	$("#img-clarinet-Csharp").show();
+				break;
 		    case "button-clarinet-D":
 		    	console.log("D note");
 		    	$("#img-clarinet-D").show();
 		    	break;    
+		    case "button-clarinet-D#":
+		    	console.log("D# note");
+		    	$("#img-clarinet-Dsharp").show();
+				break;
 		  	case "button-clarinet-E":
 		    	$("#img-clarinet-E").show();
 		    	break;
 		  	case "button-clarinet-F":
 		    	$("#img-clarinet-F").show();
 		    	break;
+		    case "button-clarinet-F#":
+		    	console.log("F# note");
+		    	$("#img-clarinet-Fsharp").show();
+				break;		
+			// No case for "G"; played open
+		    case "button-clarinet-G#":
+		    	console.log("G# note");
+		    	$("#img-clarinet-Gsharp").show();
+				break;
+		    case "button-clarinet-A":
+		    	console.log("A note");
+		    	$("#img-clarinet-A").show();
+				break;
+
+		    case "button-clarinet-A#":
+		    	console.log("A# note");
+		    	$("#img-clarinet-Asharp").show();
+				break;
+
+		    case "button-clarinet-B":
+		    	console.log("B note");
+		    	$("#img-clarinet-B").show();
+				break;
+
+
 			// var notes = {
 			// 	"button-clarinet-C": "$('#img-clarinet-C').show();",
 			// 	"button-clarinet-D": "$('#img-clarinet-D').show();"
