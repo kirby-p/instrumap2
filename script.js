3d89f46a687831572f2c34f6b8784c96a6aa2063
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-09-20 21:47:17 -0400

    Styles for home page

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ app.config(function($routeProvider) {
 
 app.controller('mainController', function($scope) {
 	$scope.title = 'Home';
-	$scope.message = 'Welcome to the main page!';
+	$scope.message = 'Welcome to Instrumap!  This is an interactive tool and central location for beginning musicians to find fingering charts for their instrument.';
+	$scope.instructions = 'Click on the compass above to search for your instrument.  Then select the note on the keyboard for the fingering you would like to learn.';
 });
 
 app.controller('keyboardController', function($scope) {
